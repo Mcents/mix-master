a6a3c6bf449e2c41971418c0dd19489895863a28
Michael Centrelli <Acentrelli@gmail.com>
2017-07-13 10:23:35 -0600

    Test

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -20,4 +20,41 @@ RSpec.describe PlaylistsController, type: :controller do
       expect(response).to render_template("show")
     end
   end
+
+  describe "GET #new" do
+    it "assigns a new playlist as @playlist" do
+      get(:new)
+
+      expect(assigns(:playlist)).to be_a_new(Playlist)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested playlist as @playlist" do
+      playlist = create(:playlist)
+      get(:edit, params: {id: playlist.id})
+      expect(assigns(:playlist)).to eq(playlist)
+    end
+  end
+
+  describe "POST #create" do
+   context "with valid params" do
+     it "creates a new artist" do
+       expect {
+         post :create, params:{:playlist => attributes_for(:playlist)}
+       }.to change(Playlist, :count).by(1)
+     end
+
+     it "assigns a newly created artist as @artist" do
+       post :create, params:{:playlist => attributes_for(:playlist)}
+       expect(assigns(:playlist)).to be_a(Playlist)
+       expect(assigns(:playlist)).to be_persisted
+     end
+
+     it "redirects to the created artist" do
+       post :create, params:{:playlist => attributes_for(:playlist)}
+       expect(response).to redirect_to(Playlist.last)
+      end
+    end
+  end
 end
